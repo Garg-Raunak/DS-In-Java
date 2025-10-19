a4b982ee02a974e33621659cfb07ff127c5bb7fa
Raunakgarg.dev <gargraunak18@gmail.com>
2025-10-19 23:46:20 +0530

    Updating Two Sum with main method

--- TwoSum.java
+++ TwoSum.java
@@ -1,17 +1,26 @@
 import java.util.*;
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
+
+class TwoSum {
+
+    public static void main(String[] args) {
+        int[] arr = twoSum(new int[] { 1, 2, 3, 4, 5, 6 }, 10);
+        for (int a : arr) {
+            System.out.println(a);
+        }
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> h1 = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int diff = target - nums[i];
 
             if (h1.containsKey(diff)) {
-                return new int[]{h1.get(diff),i};
+                return new int[] { h1.get(diff), i };
             } else {
                 h1.put(nums[i], i);
             }
         }
-        return new int[]{};
+        return new int[] {};
 
     }
 }
