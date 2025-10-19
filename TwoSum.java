6b8df1e732bfe6f24ee1b13419bf8ac6aa2d24f4
Raunakgarg.dev <gargraunak18@gmail.com>
2025-10-19 23:33:21 +0530

    Committing TwoSum.java

---
+++ TwoSum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> h1 = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+
+            if (h1.containsKey(diff)) {
+                return new int[]{h1.get(diff),i};
+            } else {
+                h1.put(nums[i], i);
+            }
+        }
+        return new int[]{};
+
+    }
+}
