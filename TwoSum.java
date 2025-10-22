3f3c2c82328113c1764d02f7b0238345bbfbad37
Raunakgarg.dev <gargraunak18@gmail.com>
2025-10-22 22:36:25 +0530

    Updating the test case

--- TwoSum.java
+++ TwoSum.java
@@ -3,7 +3,7 @@ import java.util.*;
 class TwoSum {
 
     public static void main(String[] args) {
-        int[] arr = twoSum(new int[] { 1, 3,4,5 }, 10);
+        int[] arr = twoSum(new int[] { 1, 3,4,5,1,1 }, 10);
         for (int a : arr) {
             System.out.println(a);
         }
